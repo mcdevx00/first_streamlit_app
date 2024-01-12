6b275bb188233186916b2b258e9a9b75a18a9aac
mcdevx00 <156459678+mcdevx00@users.noreply.github.com>
2024-01-12 19:12:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include  . Choose a Few Fruits to Set a Good Example
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-streamlit.print("TEST")
+streamlit.text(fruits_selected)
+streamlit.text('----TEST---')
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
