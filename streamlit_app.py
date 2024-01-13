04e2794a05bd3f54479897d9af0f79aded302515
mcdevx00 <156459678+mcdevx00@users.noreply.github.com>
2024-01-13 17:41:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #Write Code to Add Rows to Our Fruit List in Snowflake
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding,' add_my_fruit)
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
