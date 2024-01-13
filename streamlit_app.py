8050bb64acf3c5bbca877341feca830f39024c8b
mcdevx00 <156459678+mcdevx00@users.noreply.github.com>
2024-01-13 17:52:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,15 +39,20 @@ streamlit.dataframe(fruits_to_show)
 
 #Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 streamlit.header('FruityVice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    # take the json version of response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-# take the json version of response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+    # output it to the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # dont run anything past here while we troubleshooting
 streamlit.stop()
